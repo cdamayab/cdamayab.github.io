6e44b971ec2bab08a8786b2a4ef6e83a5b72a180
cdamayab <cdamayab@gmail.com>
2021-07-27 14:22:31 -0500

    Add full page scroll style

--- js/com_presentacion.js
+++ js/com_presentacion.js
@@ -2,7 +2,7 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
+class Cover extends React.Component {
   constructor(props) {
     super(props);
     this.state = { liked: false };
@@ -21,5 +21,5 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+//const domContainer = document.querySelector('#like_button_container');
+//ReactDOM.render(e(LikeButton), domContainer);
