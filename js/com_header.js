e5237bbe88c737d3519e6429ab37b06f5b9b950d
cdamayab <cdamayab@gmail.com>
2021-08-06 11:33:19 -0500

    Background changed to particles canvas animation

--- js/com_header.js
+++ js/com_header.js
@@ -13,15 +13,22 @@ class header extends React.Component {
       return 'You liked this.';
     }
 
+    var title = 'Cristhian Amaya';
+    var parr='Soy una persona dinamica, proactiva, con buenas habilidades tecnicas escribiendo codigo en varios lenguajes de programacion, tengo \
+          experiencia en el desarrollo web front-end (HTML, JavaScript, CSS) y conocimientos en lenguajes back-end (PHP, Java, Python)';
+    const skills=["HTML","CSS","JavaScript", "PHP", "JAVA", "SQL", "C++", "Python"];
+
     return e('div',{ id: 'particles'},
-		e('div',{ id: 'intro'},
-	    	    [
-			e('h1',{onClick: () => this.setState({ liked: true })},'Cristhian Amaya'),
-			e('p',{},'Desarrollador con conocimiento en lenguajes de programacion web front-end (HTML, JavaScript, CSS) y en back-end (PHP, Java, Python)')
-         	    ]
-		)
-    	   );
-      
+      e('div',{ id: 'intro'},
+        [
+          e('h1',{onClick: () => this.setState({ liked: true })}, title),
+          e('p',{}, parr),
+          e('div',{}, 
+            skills.map(element => {return e('button',{ className: 'btn btn-outline-dark'}, element)})
+          )
+        ]
+      )
+    );  
   }
 }
 
