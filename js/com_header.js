ee980f4d29bd4f0327fbd1a08bf2589998092cca
cdamayab <cdamayab@gmail.com>
2021-08-10 00:13:54 -0500

    Fixed header

--- js/com_header.js
+++ js/com_header.js
@@ -18,17 +18,30 @@ class header extends React.Component {
           experiencia en el desarrollo web front-end (HTML, JavaScript, CSS) y conocimientos en lenguajes back-end (PHP, Java, Python)';
     const skills=["HTML","CSS","JavaScript", "PHP", "JAVA", "SQL", "C++", "Python"];
 
-    return e('div',{ id: 'particles'},
-      e('div',{ id: 'intro'},
+    return [
+      e('div',{ className:'pan', id: 'pan-left'},
         [
-          e('h1',{onClick: () => this.setState({ liked: true })}, title),
-          e('p',{}, parr),
-          e('div',{}, 
-            skills.map(element => {return e('button',{ className: 'btn btn-outline-dark'}, element)})
-          )
+          e('div',{ id: 'intro'},
+            [
+              e('h1',{onClick: () => this.setState({ liked: true })}, title),
+              e('p',{}, parr),
+              e('div',{}, 
+                skills.map(element => {return e('button',{ className: 'btn btn-outline-dark'}, element)})
+              )
+            ]
+          ),
+          e('div',{ id: 'social'},
+            [
+              e('a',{ className:'fa fa-facebook-square'},''),
+              e('a',{ className:'fa fa-linkedin-square'},''),
+              e('a',{ className:'fa fa-github-square'},'')
+            ]
+          ),
+
         ]
-      )
-    );  
+      ),
+      e('div',{ className:'pan', id: 'pan-right'},"div2")
+    ];
   }
 }
 
